aebedfe0f9739f5101814bda90d970e5c05a022f
bluepeartree <sherrie.sebastian@gmail.com>
2017-10-30 14:28:27 -0400

    exercises 1-3 complete!

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,19 @@
 puts "What is your name?"
+
+user_name = gets
+
+puts "Hello, #{user_name}!"
+
+puts "Wait, what was it again?"
+
+user_name = gets.chomp
+
+puts "How old are you, #{user_name}?"
+
+age = gets
+
+age_numeric = age.to_i
+
+birth_year = 2017 - age_numeric
+
+puts "Wow, you were born in #{birth_year}!"
